df08f0fb81eef03df79cf5ac674d54b7903312fc
leegwae <leegwae@gmail.com>
2022-03-09 01:15:26 +0900

    feat: fetch products dummy data

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,17 @@
-const ProductList = () => <div>상품 리스트</div>;
+import { useQuery } from "react-query";
+import { fetcher, QueryKeys } from "../../queryClient";
+
+const ProductList = () => {
+	const { data } = useQuery(QueryKeys.PRODUCTS, () =>
+		fetcher({
+			method: 'GET',
+			path: '/products',
+		})
+	);
+
+	console.log(data);
+
+	return <div>상품 목록</div>;
+}
 
 export default ProductList;
