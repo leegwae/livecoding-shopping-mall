480524284cc7b2deeb0583f2d91fea9621b6fd60
leegwae <leegwae@gmail.com>
2022-03-09 01:05:13 +0900

    feat: add fetcher and QueryKeys

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,10 +1,6 @@
-import {
-	useQuery,
-	useMutation,
-	useQueryClient,
-	QueryClient,
-	QueryClientProvider,
-} from 'react-query';
+import {	QueryClient } from 'react-query';
+
+type AnyOBJ = { [key: string]: any };
 
 export const getClient = (() => {
 	let client: QueryClient | null = null;
@@ -14,3 +10,38 @@ export const getClient = (() => {
 		return client;
 	}
 })();
+
+const BASE_URL = 'https://fakestoreapi.com';
+
+export const fetcher =async ({
+	method,
+	path,
+	body,
+	params
+}: {
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATH';
+	path: string;
+	body?: AnyOBJ;
+	params?: AnyOBJ;
+}) => {
+	try {
+		const url = `${BASE_URL}${path}`;
+		const fetchOptions: RequestInit = {
+			method,
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': BASE_URL,
+			}
+		};
+
+		const res = await fetch(url, fetchOptions);
+		const json = await res.json();
+		return json;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export const QueryKeys = {
+	PRODUCTS: 'PRODUCTS',
+};
