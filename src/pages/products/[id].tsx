4319045f27767b62d65b670b11b5f7a9a2b8be7b
leegwae <leegwae@gmail.com>
2022-03-09 02:33:39 +0900

    feat: add ProductDetail component

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,3 +1,27 @@
-const ProductDetail = () => <div>상품 상세페이지</div>;
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { Product } from '../../type';
+import { fetcher, QueryKeys } from '../../queryClient';
+import ProductDetail from '../../components/product/detail';
 
-export default ProductDetail;
+const ProductDetailPage = () => {
+  const { id } = useParams();
+
+  const { data } = useQuery<Product>([QueryKeys.PRODUCTS, id], () =>
+    fetcher({
+      method: 'GET',
+      path: `/products/${id}`,
+    }),
+  );
+
+  if (!data) return null;
+
+  return (
+    <div>
+      <h2>상품 상세</h2>
+      <ProductDetail item={data} />
+    </div>
+  );
+};
+
+export default ProductDetailPage;
