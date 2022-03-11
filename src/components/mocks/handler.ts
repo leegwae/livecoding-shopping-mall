5678847e57874143fe3944e1c9323ea4b6f3278c
leegwae <leegwae@gmail.com>
2022-03-11 14:50:48 +0900

    feat: add handler for mocking graphql API

---
+++ src/components/mocks/handler.ts
@@ -0,0 +1,9 @@
+// src/mocks/handlers.js
+import { graphql } from 'msw'
+import { QueryKeys } from '../../queryClient'
+
+export const handlers = [
+  graphql.query(QueryKeys.PRODUCTS, (req, res, ctx) => {
+		
+	}),
+]
