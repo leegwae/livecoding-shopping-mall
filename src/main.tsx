628638ce27fbab5cfe8d928d8c9b78870d05faf1
leegwae <leegwae@gmail.com>
2022-03-11 16:10:02 +0900

    feat: import wokrer for starting it

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
+import { worker } from './mocks/browser';
+
+// https://mswjs.io/docs/getting-started/integrate/browser#start-worker
+// https://vitejs.dev/guide/env-and-mode.html#env-variables
+if (import.meta.env.DEV) {
+  worker.start()
+}
 
 ReactDOM.render(
   <React.StrictMode>
