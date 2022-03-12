230e12d08ee80c71fc423dd16d719747bcb2c660
leegwae <leegwae@gmail.com>
2022-03-12 21:59:31 +0900

    feat: wrap app with recoil root

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './scss/index.scss';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { useRoutes } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import { routes } from './routes';
 import { getClient } from './queryClient';
 import Gnb from './components/gnb';
@@ -11,11 +12,14 @@ const App = () => {
 	const queryClient = getClient();
 
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Gnb />
-			{element}
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<RecoilRoot>
+			<QueryClientProvider client={queryClient}>
+				<Gnb />
+				{element}
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</RecoilRoot>
+		
 	);
 };
 
