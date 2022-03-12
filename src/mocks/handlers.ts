0c1c554119952862360dc86821109999dde164d9
leegwae <leegwae@gmail.com>
2022-03-13 00:48:21 +0900

    feat: add handlers for cart system

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,28 +1,59 @@
-import { graphql } from 'msw';
-import { v4 as uuid } from 'uuid';
-import GET_PRODUCTS, { GET_PRODUCT } from '../graphql/products';
+import { graphql } from 'msw'
+import { GET_CART, ADD_CART, CartType } from '../graphql/cart'
+import GET_PRODUCTS, { GET_PRODUCT } from '../graphql/products'
 
-const mock_products = Array.from({ length: 20 }).map((_, i) => ({
-	id: uuid(),
-	imageUrl: `https://placeimg.com/300/500/${i+1}`,
-	price: 50000,
-	title: `임시 상품 ${i+1}`,
-	descrption: `임시 상세내용 ${i+1}`,
-	createAt: new Date(1634567890123+(i*1000*60*60*11)).toString()
-}));
+const mockProducts = (() =>
+  Array.from({ length: 20 }).map((_, i) => ({
+    id: i + 1 + '',
+    imageUrl: `https://placeimg.com/200/150/${i + 1}`,
+    price: 50000,
+    title: `임시상품${i + 1}`,
+    description: `임시상세내용${i + 1}`,
+    createAt: new Date(1634567890123+(i*1000*60*60*11)).toString(),
+  })))();
+
+let cartData: { [key: string]: CartType } = {}
+
+const handlers_product = [
+	graphql.query(GET_PRODUCTS, (req, res, ctx) => {
+    return res(ctx.data({ products: mockProducts }));
+  }),
+  graphql.query(GET_PRODUCT, (req, res, ctx) => {
+    const found = mockProducts.find(item => item.id === req.variables.id)
+    if (!found) return res();
+    return res(ctx.data(found));
+  }),
+];
+
+const handlers_cart = [
+  graphql.query(GET_CART, (req, res, ctx) => {
+    return res(ctx.data(cartData));
+  }),
+  graphql.mutation(ADD_CART, (req, res, ctx) => {
+    const newData = { ...cartData };
+    const id = req.variables.id
+
+    if (newData[id]) {
+      newData[id] = {
+        ...newData[id],
+        amount: (newData[id].amount || 0) + 1,
+      };
+    } else {
+      const found = mockProducts.find(item => item.id === req.variables.id);
+      if (found) {
+        newData[id] = {
+          ...found,
+          amount: 1,
+        };
+      }
+    }
+
+    cartData = newData;
+    return res(ctx.data(newData));
+  }),
+];
 
 export const handlers = [
-  graphql.query(GET_PRODUCTS, (req, res, ctx) => {
-		return res(
-			ctx.data({
-				products: mock_products
-			}),
-		)
-	}),
-
-	graphql.query(GET_PRODUCT, (req, res, ctx) => {
-		return res(
-			ctx.data(mock_products[0])
-		)
-	})
+	...handlers_product,
+	...handlers_cart,
 ];
