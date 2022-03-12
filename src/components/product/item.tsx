4e98aa651bcf3eb471723b33a8e48f493ba1c33a
leegwae <leegwae@gmail.com>
2022-03-12 23:29:33 +0900

    feat: display amount of product added to cart

--- src/components/product/item.tsx
+++ src/components/product/item.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useRecoilValue, useRecoilState, SetRecoilState } from 'recoil';
+import { cartItemAmountQuery } from "../../atom/cart";
 import { Product } from "../../graphql/products";
 
 const ProductItem = ({
@@ -9,6 +11,10 @@ const ProductItem = ({
 	description,
 	createAt,
 }: Product) => {
+	const [cartAmount, setCartAmount] = useRecoilState(cartItemAmountQuery(id));
+
+	const addToCart = () => setCartAmount((cartAmount || 0) + 1);
+
 	return (
 		<li className="product-item">
 			<Link to={`/products/${id}`}>
@@ -16,7 +22,8 @@ const ProductItem = ({
 				<img className="product-item__image" src={imageUrl} />
 				<span className="product-item__price">${price}</span>
 			</Link>
-			<button className="product-item__add-to-cart">+</button>
+			<button className="product-item__add-to-cart" onClick={addToCart}>+</button>
+			<span>{cartAmount || 0}</span>
  		</li>		
 	);
 }
