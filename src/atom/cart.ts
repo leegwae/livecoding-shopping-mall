416bf5c65b6dda5fa6ba93a7ae0ad897b4e68fcf
leegwae <leegwae@gmail.com>
2022-03-13 21:16:31 +0900

    feat: create atom to manage checked item on cart

--- src/atom/cart.ts
+++ src/atom/cart.ts
@@ -1,20 +1,26 @@
 import { atom, selectorFamily } from 'recoil';
+import { CartType } from '../graphql/cart';
 
-const cartListState = atom<Map<string, number>>({
-	key: 'CartList',
-	default: new Map(),
+export const checkedCartState = atom<CartType[]>({
+	key: 'CheckedCart',
+	default: [],
 });
 
-export const cartItemAmountQuery = selectorFamily<number | undefined, string>({
-	key: 'CartItemQuery',
-	get: (cartItemId: string) => ({ get }) => {
-		return get(cartListState).get(cartItemId)
-	},
-	set: (cartItemId: string) => ({ get, set }, newValue) => {
-		if (typeof newValue === 'number') {
-			const newCartList = new Map(get(cartListState));
-			newCartList.set(cartItemId, newValue);
-			set(cartListState, newCartList);
-		}
-	}
-});
+// const cartListState = atom<Map<string, number>>({
+// 	key: 'CartList',
+// 	default: new Map(),
+// });
+
+// export const cartItemAmountQuery = selectorFamily<number | undefined, string>({
+// 	key: 'CartItemQuery',
+// 	get: (cartItemId: string) => ({ get }) => {
+// 		return get(cartListState).get(cartItemId)
+// 	},
+// 	set: (cartItemId: string) => ({ get, set }, newValue) => {
+// 		if (typeof newValue === 'number') {
+// 			const newCartList = new Map(get(cartListState));
+// 			newCartList.set(cartItemId, newValue);
+// 			set(cartListState, newCartList);
+// 		}
+// 	}
+// });
