391104904e666a6ec722f40738a0dfc7118b5630
leegwae <leegwae@gmail.com>
2022-03-13 13:10:53 +0900

    feat: use graphql fetcher to update cart

--- src/components/cart/item.tsx
+++ src/components/cart/item.tsx
@@ -1,12 +1,64 @@
-import React from 'react'
-import { CartType } from '../../graphql/cart';
+import React, { SyntheticEvent } from 'react'
+import { useMutation } from 'react-query';
+import { stringify } from 'uuid';
+import { CartType, UPDATE_CART } from '../../graphql/cart';
+import { getClient, graphqlFetcher, QueryKeys } from '../../queryClient';
 
-const CartItem = ({ item }: { item: CartType }) => {
+const CartItem = ({
+	id,
+	imageUrl,
+	price,
+	title,
+	amount,
+}: CartType ) => {
+	const queryClient = getClient();
+	const { mutate: updateCart } = useMutation(
+    ({ id, amount }: { id: string; amount: number }) => graphqlFetcher(UPDATE_CART, { id, amount }),
+		{
+			onMutate: async (newValue) => {
+				const { id, amount } = newValue; 
+
+				await queryClient.cancelQueries(QueryKeys.CART);
+				const prevCart = queryClient.getQueryData<{ [key: string]: CartType} >(QueryKeys.CART);
+				
+				if (!prevCart?.[id]) return prevCart;
+
+				const newCart = {...(prevCart || []), [id]: { ...prevCart[id], amount }};
+				queryClient.setQueryData(QueryKeys.CART, newCart);
+				
+				return prevCart;
+			},
+			onSuccess: newValue => {
+        const prevCart = queryClient.getQueryData<{ [key: string]: CartType }>(QueryKeys.CART);
+        const newCart = {
+          ...(prevCart || {}),
+          [id]: newValue,
+        };
+        queryClient.setQueryData(QueryKeys.CART, newCart);
+      },
+		}
+	);
+
+	const handleUpdateAmount = (e: SyntheticEvent) => {
+		const amount = Number((e.target as HTMLInputElement).value);
+		updateCart({ id, amount })
+		
+	};
+
 	return (
-		<li>
-			<span>{item.title}	</span>
-			<span>{item.amount}</span>
-		</li>
+    <li className="cart-item">
+      <input className="cart-item__checkbox" type="checkbox" name="select-item" />
+      <img className="cart-item__image" src={imageUrl} />
+      <p className="cart-item__price">{price}</p>
+      <p className="cart-item__title">{title}</p>
+			<input
+        className="cart-item__amount"
+        type="number"
+        value={amount}
+        min={1}
+        onChange={handleUpdateAmount}
+      />
+    </li>
 	);
 }
 
