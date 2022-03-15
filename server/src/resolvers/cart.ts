1496cec6ea0363405f0dd0c0e0981d56a7b540e7
leegwae <leegwae@gmail.com>
2022-03-15 20:36:49 +0900

    feat: define resolver

---
+++ server/src/resolvers/cart.ts
@@ -0,0 +1,64 @@
+const mockProducts = (() =>
+  Array.from({ length: 20 }).map((_, i) => ({
+    id: i + 1 + '',
+    imageUrl: `https://picsum.photos/id/${i + 20}/200/150`,
+    price: 50000,
+    title: `임시상품${i + 1}`,
+    description: `임시상세내용${i + 1}`,
+    createAt: new Date(1634567890123+(i*1000*60*60*11)).toString(),
+  })))();
+
+let cartData = [{ id: '1', amount: 1 }, { id: '2', amount: 2 }];
+
+const cartResolver = {
+	Query: {
+		cart: (parent, args, context, info) => {
+			return cartData;
+		},
+	},
+	Mutation: {
+		addCart: (parent, { id }, context, info) => {
+			const newCartData = { ...cartData };
+			const targetProduct = mockProducts.find(item => item.id === id);
+	
+			if (!targetProduct) { throw new Error('상품이 없습니다'); }
+	
+			const newItem = {
+				...targetProduct,
+				amount: (newCartData[id]?.amount || 0) + 1,
+			}
+			newCartData[id] = newItem;
+			cartData = newCartData;
+	
+			return newItem;
+		},
+		updateCart: (parent, { id, amount }, context, info)  => {
+			const newData = { ...cartData };
+			if (!newData[id]) { throw new Error('없는 데이터입니다'); }
+	
+			const newItem = {
+				...newData[id],
+				amount,
+			};
+			newData[id] = newItem;
+			cartData = newData;
+	
+			return newItem;
+		},
+		deleteCart: (parent, { id }, context, info) => {
+			const newData = { ...cartData };
+			delete newData[id];
+			cartData = newData;
+
+			return id; 
+		},
+		executeCart: (parent, { ids }, context, info) => {
+			const newCartdata = cartData.filter(cartItem => !ids.includes(cartItem.id));
+			cartData = newCartdata;
+
+			return ids;
+		},
+	}
+}
+
+export default cartResolver;
