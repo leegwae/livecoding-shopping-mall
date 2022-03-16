bedb9b9996ecca18cde3c5b477fd748b7b7c49a8
leegwae <leegwae@gmail.com>
2022-03-17 00:09:01 +0900

    feat: redefine graphql query for cart

--- client/src/graphql/cart.ts
+++ client/src/graphql/cart.ts
@@ -1,51 +1,67 @@
 import gql from "graphql-tag";
+import { Product, Products } from "./products";
 
 export type CartType = {
 	id: string;
-	imageUrl: string;
-	price: number;
-	title: string;
+	product: Product;
 	amount: number;
 }
-
-export const UPDATE_CART = gql`
-  mutation UPDATE_CART($id: string, $amount: number) {
-    cart(id: $id, amount: $number) {
-      id
-      imageUrl
-      price
-      title
-      amount
-    }
-  }
-`
 
 export const GET_CART = gql`
   query GET_CART {
     cart {
       id
-      imageUrl
-      price
-      title
+      product {
+        id
+        imageUrl
+        price
+        title
+        description
+        createdAt
+      }
       amount
     }
   }
 `;
 
-export const ADD_CART = gql`
-  mutation ADD_CART($id: string) {
-    cart(id: $id) {
+export const UPDATE_CART = gql`
+  mutation UPDATE_CART($id: ID!, $amount: Int!) {
+    updateCart(id: $id, amount: $amount) {
       id
-      imageUrl
-      price
-      title
+      product {
+        id
+        imageUrl
+        price
+        title
+        description
+        createdAt
+      }
+      amount
+    }
+  }
+`
+
+export const ADD_CART = gql`
+  mutation Mutation($id: ID!) {
+    addCart(id: $id) {
+      id
+      product {
+        id
+        imageUrl
+        price
+        title
+        description
+        createdAt
+      }
       amount
     }
   }
 `
 
 export const DELETE_CART = gql`
-  mutation DELETE_CART($id: string) {
-    id
+  mutation DELETE_CART($id: ID!) {
+    deleteCart {
+      id
+    }
   }
 `;
