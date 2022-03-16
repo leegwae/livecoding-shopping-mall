0b744f5d4dc0f0842b8669dad9830464705ab8ba
leegwae <leegwae@gmail.com>
2022-03-16 23:17:18 +0900

    remove file using msw from client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app';
-import { worker } from './mocks/browser';
+import App from './App';
 
-// https://mswjs.io/docs/getting-started/integrate/browser#start-worker
 // https://vitejs.dev/guide/env-and-mode.html#env-variables
-if (import.meta.env.DEV) {
-  worker.start()
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
--- client/src/mocks/browser.ts
+++
@@ -1,8 +0,0 @@
-// https://mswjs.io/docs/getting-started/integrate/browser#configure-worker
-
-// src/mocks/browser.js
-import { setupWorker } from 'msw';
-import { handlers } from './handlers';
-
-// This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers);
