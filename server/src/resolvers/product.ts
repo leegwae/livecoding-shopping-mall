bb3011b31c6d159cd0fe16021511d327ec04fb5a
leegwae <leegwae@gmail.com>
2022-03-18 23:05:07 +0900

    featL define resolvers for product mutations

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -1,4 +1,8 @@
-import { Resolver } from './types';
+import { v4 as uuid } from 'uuid';
+import { Products, Resolver } from './types';
+import { DBField, writeDB } from '../dbController';
+
+const setJSON = (data: Products) => writeDB(DBField.PRODUCTS, data);
 
 const productResolver: Resolver = {
 	Query: {
@@ -12,6 +16,47 @@ const productResolver: Resolver = {
 			return null;
 		}
 	},
+
+	Mutation: {
+    addProduct: (parent, { imageUrl, price, title, description }, { db }) => {
+      const newProduct = {
+        id: uuid(),
+        imageUrl,
+        price,
+        title,
+        description,
+        createdAt: Date.now(),
+      }
+      db.products.push(newProduct)
+      setJSON(db.products)
+      return newProduct
+    },
+		updateProduct: (parent, { id, ...data }, { db }) => {
+			const existProductIndex = db.products.findIndex(item => item.id === id);
+			if (existProductIndex < 0) throw new Error("없는 상품입니다!");
+
+			const updatedItem = {
+				...db.products[existProductIndex],
+				...data,
+			};
+			db.products.splice(existProductIndex, 1, updatedItem);
+			setJSON(db.products);
+			return updatedItem;
+		},
+		deleteProduct: (praent, { id }, { db }) => {
+			const existProductIndex = db.products.findIndex(item => item.id === id);
+			if (existProductIndex < 0) throw new Error("없는 상품입니다!");
+
+			const updatedItem = {
+				...db.products[existProductIndex]
+			};
+
+			delete updatedItem.createdAt;
+			db.products.splice(existProductIndex, 1, updatedItem);
+			setJSON(db.products);
+			return id;
+		}
+	}
 }
 
 export default productResolver;
