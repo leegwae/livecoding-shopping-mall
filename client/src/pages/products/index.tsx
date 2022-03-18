335b9f7e6f8ad68261aebde8ab68ee7c35697aad
leegwae <leegwae@gmail.com>
2022-03-18 21:40:52 +0900

    feat: IntersectionObserver로 무한스크롤링 구현(state가 바뀌지 않는 오류 있음)

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,22 +1,59 @@
-import { useQuery } from "react-query";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useInfiniteQuery } from "react-query";
 import { graphqlFetcher, QueryKeys } from "../../queryClient";
-import GET_PRODUCTS, { Product, Products } from "../../graphql/products";
+import GET_PRODUCTS, { Products } from "../../graphql/products";
 import ProductList from "../../components/product/list";
 
 const ProductListPage = () => {
-	const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () =>
-		graphqlFetcher(
-			GET_PRODUCTS
-		)
+	const observerRef = useRef<IntersectionObserver>();
+	const fetchMoreRef = useRef<HTMLDivElement>(null);
+	const [intersecting, setIntersecting] = useState(false);
+
+	const getObserver = useCallback(() => {
+		if (!observerRef.current) {
+			observerRef.current = new IntersectionObserver(entries => {
+				setIntersecting(entries[0]?.isIntersecting);
+			});
+		}
+		return observerRef.current;
+	}, [observerRef.current]);
+
+	useEffect(() => {
+		if (fetchMoreRef.current) getObserver().observe(fetchMoreRef.current);
+	}, [fetchMoreRef.current]);
+
+	const { data, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
+		QueryKeys.PRODUCTS,
+		({ pageParam = ''}) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
+		{
+			getNextPageParam: (lastPage) => {
+				return lastPage.products.at(-1)?.id;
+			}
+		}
 	);
 
+	/*
+	data: {
+		pages: {
+			{ products: [...] },
+			{ products: [...] },
+		},
+		pageParams: [undefined, ...]
+	}
+	*/
+
+	useEffect(() => {
+		if (!intersecting || !isSuccess || !hasNextPage && isFetchingNextPage) return;
+		fetchNextPage();
+		
+	}, [intersecting]);
+
 	return (
-		<>
+		<div>
 			<h1>상품 목록</h1>
-			<div>
-				<ProductList list={data?.products || []} />
-			</div>
-		</>
+			<ProductList list={data?.pages || []} />
+			<div ref={fetchMoreRef} />
+		</div>
 	);
 };
 
