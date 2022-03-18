08de71ae0c19f4f057233bf9a6e172725f91066f
leegwae <leegwae@gmail.com>
2022-03-18 22:22:09 +0900

    refactor: define useInfiniteScroll hooks from component

---
+++ client/src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,26 @@
+import { RefObject, useCallback, useRef, useState, useEffect } from "react"
+;
+const useInifniteScroll = (targetRef: RefObject<HTMLDivElement>) => { 
+	const observerRef = useRef<IntersectionObserver>();
+	const [intersecting, setIntersecting] = useState(false);
+
+	const getObserver = useCallback(
+		() => {
+			if (!observerRef.current) {
+				observerRef.current = new IntersectionObserver(entries => {
+					setIntersecting(entries[0]?.isIntersecting);
+				});
+			}
+			return observerRef.current;
+		},
+		[observerRef.current],
+	)
+	
+	useEffect(() => {
+		if (targetRef.current) getObserver().observe(targetRef.current);
+	}, [targetRef.current]);
+	
+	return intersecting;
+};
+
+export default useInifniteScroll;
--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,27 +1,14 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useInfiniteQuery } from "react-query";
 import { graphqlFetcher, QueryKeys } from "../../queryClient";
 import GET_PRODUCTS, { Products } from "../../graphql/products";
 import ProductList from "../../components/product/list";
+import useInifniteScroll from "../../hooks/useInfiniteScroll";
 
 const ProductListPage = () => {
-	const observerRef = useRef<IntersectionObserver>();
 	const fetchMoreRef = useRef<HTMLDivElement>(null);
-	const [intersecting, setIntersecting] = useState(false);
-
-	const getObserver = useCallback(() => {
-		if (!observerRef.current) {
-			observerRef.current = new IntersectionObserver(entries => {
-				setIntersecting(entries[0]?.isIntersecting);
-			});
-		}
-		return observerRef.current;
-	}, [observerRef.current]);
-
-	useEffect(() => {
-		if (fetchMoreRef.current) getObserver().observe(fetchMoreRef.current);
-	}, [fetchMoreRef.current]);
-
+	const intersecting = useInifniteScroll(fetchMoreRef);
+	
 	const { data, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
 		QueryKeys.PRODUCTS,
 		({ pageParam = ''}) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
@@ -31,16 +18,6 @@ const ProductListPage = () => {
 			}
 		}
 	);
-
-	/*
-	data: {
-		pages: {
-			{ products: [...] },
-			{ products: [...] },
-		},
-		pageParams: [undefined, ...]
-	}
-	*/
 
 	useEffect(() => {
 		if (!intersecting || !isSuccess || !hasNextPage && isFetchingNextPage) return;
