d6981945d81a94b741aa023b7da49375f926ec01
leegwae <leegwae@gmail.com>
2022-03-23 00:20:12 +0900

    refactor: Admin 컴포넌트 분리

---
+++ client/src/components/admin/index.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { graphqlFetcher, QueryKeys } from '../../queryClient';
+import GET_PRODUCTS, { Products } from '../../graphql/products';
+import ProductList from '../../components/product/list';
+import useIntersection from '../../hooks/useIntersection';
+import AddForm from '../../components/admin/addForm';
+
+const Admin = () => {
+	const fetchMoreRef = useRef<HTMLDivElement>(null);
+	const intersecting = useIntersection(fetchMoreRef);
+	
+	const { data, isSuccess, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
+		[QueryKeys.PRODUCTS, 'admin'],
+		({ pageParam = ''}) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam, showDelected: true }),
+		{
+			getNextPageParam: (lastPage) => {
+				return lastPage.products.at(-1)?.id;
+			}
+		}
+	);
+
+	useEffect(() => {
+		if (!intersecting || !isSuccess || !hasNextPage || isFetchingNextPage) return;
+		fetchNextPage();
+		
+	}, [intersecting]);
+
+	return (
+		<>
+			<AddForm />
+			<ProductList list={data?.pages || []} />
+			<div ref={fetchMoreRef} />
+		</>
+	);
+};
+
+export default Admin;
