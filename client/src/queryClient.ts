9034602f62027b1391aa0f57bd60fc434c92bf55
leegwae <leegwae@gmail.com>
2022-03-25 23:38:35 +0900

    change base url on client

--- client/src/queryClient.ts
+++ client/src/queryClient.ts
@@ -22,43 +22,7 @@ export const getClient = (() => {
 	}
 })();
 
-// export const restFetcher =async ({
-// 	method,
-// 	path,
-// 	body,
-// 	params
-// }: {
-// 	method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATH';
-// 	path: string;
-// 	body?: AnyOBJ;
-// 	params?: AnyOBJ;
-// }) => {
-// 	try {
-// 		let url = `${BASE_URL}${path}`;
-// 		const fetchOptions: RequestInit = {
-// 			method,
-// 			headers: {
-// 				'Content-Type': 'application/json',
-// 				'Access-Control-Allow-Origin': BASE_URL,
-// 			}
-// 		};
-
-// 		if (params) {
-// 			const searchParams = new URLSearchParams(params);
-// 			url += `?${searchParams.toString()}`
-// 		}
-
-// 		if (body) fetchOptions.body = JSON.stringify(body);
-
-// 		const res = await fetch(url, fetchOptions);
-// 		const json = await res.json();
-// 		return json;
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// }
-
-const BASE_URL = 'http://localhost:8000/graphql'
+const BASE_URL = 'https://lee-shop.herokuapp.com/graphql'
 export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
 	request(BASE_URL, query, variables);
 
